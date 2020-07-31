8882beb8ed42b12a533683c0a5ff93433f1d9b7a
Adriano Gil <adrianogil.san@gmail.com>
2020-07-31 07:15:05 -0400

    Minor improvements on portscan command

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
-
 import socket
+import sys
 
-ip = raw_input("Enter target IP:\n")
-for porta in range(1,65535):
+
+if len(sys.argv) == 1:
+    ip = input("Enter target IP:\n")
+else:
+    ip = sys.argv[1]
+
+print("Using IP: %s" % (ip,))
+
+open_ports = []
+
+for port in range(1, 65535):
+    print("[debug] Testing port %s" % (port,))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if s.connect_ex((ip,porta)) == 0:
-        print porta, "OPEN"
+    if s.connect_ex((ip, port)) == 0:
+        open_ports.append(port)
     s.close()
+
+print("The following ports are open:")
+for port in open_ports:
+    print("%s" % (port,))
